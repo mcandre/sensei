cd765f86dfe76ce9dc4440b35ba3ed6924e6ad97
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-07-29 16:22:36 -0500

    pep8

--- sensei.py
+++ sensei.py
@@ -11,103 +11,136 @@ import os
 import time
 
 ENGLISH = {
-  "kao": "face",
-  "me": "eyes",
-  "hana": "nose"
-  }
+    "kao": "face",
+    "me": "eyes",
+    "hana": "nose"
+}
+
 
 def main():
-  camera = SimpleCV.Camera()
+    camera = SimpleCV.Camera()
 
-  d = os.path.abspath(os.path.dirname(sys.argv[0])) + os.sep
+    d = os.path.abspath(os.path.dirname(sys.argv[0])) + os.sep
 
-  haar_hand = d + "haar-hand.xml"
+    haar_hand = d + "haar-hand.xml"
 
-  lessons = [
-    "kao", # face
-    "me", # eyes
-    "hana" # nose
+    lessons = [
+        "kao",  # face
+        "me",   # eyes
+        "hana"  # nose
     ]
 
-  lesson = random.choice(lessons)
+    lesson = random.choice(lessons)
 
-  haar = d + "haar-" + ENGLISH[lesson] + ".xml"
+    haar = d + "haar-" + ENGLISH[lesson] + ".xml"
 
-  image = camera.getImage()
-  w, h = image.size()
-
-  kao_pos = [w - 100, 100]
-  me_pos = [w - 100, h / 2]
-  hana_pos = [w - 100, h - 100]
-  result_pos = [100, h - 100]
-  next_lesson_pos = [100, 100]
-
-  default_color = SimpleCV.Color.GREEN
-  select_color = SimpleCV.Color.BLUE
-  result_color = SimpleCV.Color.RED
-  next_lesson_color = SimpleCV.Color.GREEN
-
-  while True:
     image = camera.getImage()
-    image = image.flipHorizontal()
+    w, h = image.size()
 
-    features = image.findHaarFeatures(haar)
+    kao_pos = [w - 100, 100]
+    me_pos = [w - 100, h / 2]
+    hana_pos = [w - 100, h - 100]
+    result_pos = [100, h - 100]
+    next_lesson_pos = [100, 100]
 
-    hands = image.findHaarFeatures(haar_hand)
+    default_color = SimpleCV.Color.GREEN
+    select_color = SimpleCV.Color.BLUE
+    result_color = SimpleCV.Color.RED
+    next_lesson_color = SimpleCV.Color.GREEN
 
-    colors = {
-      "kao": default_color,
-      "me": default_color,
-      "hana": default_color,
-      "nextlesson": next_lesson_color
-      }
+    while True:
+        image = camera.getImage()
+        image = image.flipHorizontal()
 
-    result = ""
+        features = image.findHaarFeatures(haar)
 
-    try:
-      features.draw(default_color)
-      hand = hands[0]
-      hand.draw(select_color)
+        hands = image.findHaarFeatures(haar_hand)
 
-      if hand.distanceFrom((kao_pos[0], kao_pos[1])) < 100:
-        colors["kao"] = select_color
-        if lesson == "kao":
-          result = "hai"
-        else:
-          result = "ie"
-      elif hand.distanceFrom((me_pos[0], me_pos[1])) < 100:
-        colors["me"] = select_color
-        if lesson == "me":
-          result = "hai"
-        else:
-          result = "ie"
-      elif hand.distanceFrom((hana_pos[0], hana_pos[1])) < 100:
-        colors["hana"] = select_color
-        if lesson == "hana":
-          result = "hai"
-        else:
-          result = "ie"
-      elif hand.distanceFrom((next_lesson_pos[0], next_lesson_pos[1])) < 100:
-        colors["nextlesson"] = select_color
-        lesson = random.choice(lessons)
-        haar = d + "haar-" + ENGLISH[lesson] + ".xml"
-    except:
-      pass
-    finally:
-      image.drawText("kao", kao_pos[0], kao_pos[1], colors["kao"], 40)
-      image.drawText("me", me_pos[0], me_pos[1], colors["me"], 40)
-      image.drawText("hana", hana_pos[0], hana_pos[1], colors["hana"], 40)
+        colors = {
+            "kao": default_color,
+            "me": default_color,
+            "hana": default_color,
+            "nextlesson": next_lesson_color
+        }
 
-      if result != "":
-        image.drawText(result, result_pos[0], result_pos[1], result_color, 40)
-        image.drawText(">", next_lesson_pos[0], next_lesson_pos[1], colors["nextlesson"], 40)
+        result = ""
 
-        image.show()
+        try:
+            features.draw(default_color)
+            hand = hands[0]
+            hand.draw(select_color)
 
-        time.sleep(0.1)
+            if hand.distanceFrom((kao_pos[0], kao_pos[1])) < 100:
+                colors["kao"] = select_color
+                if lesson == "kao":
+                    result = "hai"
+                else:
+                    result = "ie"
+            elif hand.distanceFrom((me_pos[0], me_pos[1])) < 100:
+                colors["me"] = select_color
+                if lesson == "me":
+                    result = "hai"
+                else:
+                    result = "ie"
+            elif hand.distanceFrom((hana_pos[0], hana_pos[1])) < 100:
+                colors["hana"] = select_color
+                if lesson == "hana":
+                    result = "hai"
+                else:
+                    result = "ie"
+            elif hand.distanceFrom(
+                (next_lesson_pos[0], next_lesson_pos[1])
+            ) < 100:
+                colors["nextlesson"] = select_color
+                lesson = random.choice(lessons)
+                haar = d + "haar-" + ENGLISH[lesson] + ".xml"
+        except:
+            pass
+        finally:
+            image.drawText(
+                "kao",
+                kao_pos[0],
+                kao_pos[1],
+                colors["kao"],
+                40
+            )
+            image.drawText(
+                "me",
+                me_pos[0],
+                me_pos[1],
+                colors["me"],
+                40
+            )
+            image.drawText(
+                "hana",
+                hana_pos[0],
+                hana_pos[1],
+                colors["hana"],
+                40
+            )
+
+            if result != "":
+                image.drawText(
+                    result,
+                    result_pos[0],
+                    result_pos[1],
+                    result_color,
+                    40
+                )
+                image.drawText(
+                    ">",
+                    next_lesson_pos[0],
+                    next_lesson_pos[1],
+                    colors["nextlesson"],
+                    40
+                )
+
+                image.show()
+
+                time.sleep(0.1)
 
 if __name__ == "__main__":
-  try:
-    main()
-  except KeyboardInterrupt:
-    pass
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
--- tasks.py
+++ tasks.py
@@ -1,25 +1,73 @@
 from invoke import run, task
 
+
 @task
 def pep8():
-  run('pep8 .')
+    run('pep8 .')
+
 
 @task
 def pylint():
-  run("pylint *.py")
+    run("pylint *.py")
+
 
 @task
 def pyflakes():
-  run("pyflakes .")
+    run("pyflakes .")
+
 
 @task
 def editorconfig():
-  run('find . -type f -name Thumbs.db -prune -o -type f -name .DS_Store -prune -o -type d -name .git -prune -o -type f -name .gitmodules -prune -o -type d -name .hg -prune -o -type d -name .svn -prune -o -type d -name tmp -prune -o -type d -name bin -prune -o -type d -name target -prune -o -name "*.app*" -prune -o -type d -name node_modules -prune -o -type d -name bower_components -prune -o -type f -name "*[-.]min.js" -prune -o -type d -name "*.dSYM" -prune -o -type f -name "*.scpt" -prune -o -type d -name "*.xcodeproj" -prune -o -type d -name .vagrant -prune -o -type f -name .exe -prune -o -type f -name "*.o" -prune -o -type f -name "*.pyc" -prune -o -type f -name "*.hi" -prune -o -type f -name "*.beam" -prune -o -type f -name "*.png" -prune -o -type f -name "*.gif" -prune -o -type f -name "*.jp*g" -prune -o -type f -name "*.ico" -prune -o -type f -name "*.ttf" -prune -o -type f -name "*.zip" -prune -o -type f -name "*.jar" -prune -o -type f -name "*.dot" -prune -o -type f -name "*.pdf" -prune -o -type f -name "*.wav" -prune -o -type f -name "*.mp[34]" -prune -o -type f -name "*.svg" -prune -o -type f -name "*.flip" -prune -o -type f -name "*.class" -prune -o -type f -name "*.gem" -prune -o -type f -name "*.jad" -prune -o -type d -name .idea -prune -o -type f -name "*.iml" -prune -o -type f -name "*.log" -prune -o -type f -name "*" -exec node_modules/.bin/editorconfig-tools check {} \\;')
+    run('find . -type f -name Thumbs.db'
+        ' -prune -o -type f -name .DS_Store'
+        ' -prune -o -type d -name .git'
+        ' -prune -o -type f -name .gitmodules'
+        ' -prune -o -type d -name .hg'
+        ' -prune -o -type d -name .svn'
+        ' -prune -o -type d -name tmp'
+        ' -prune -o -type d -name bin'
+        ' -prune -o -type d -name target'
+        ' -prune -o -name "*.app*"'
+        ' -prune -o -type d -name node_modules'
+        ' -prune -o -type d -name bower_components'
+        ' -prune -o -type f -name "*[-.]min.js"'
+        ' -prune -o -type d -name "*.dSYM"'
+        ' -prune -o -type f -name "*.scpt"'
+        ' -prune -o -type d -name "*.xcodeproj"'
+        ' -prune -o -type d -name .vagrant'
+        ' -prune -o -type f -name .exe -prune'
+        ' -o -type f -name "*.o"'
+        ' -prune -o -type f -name "*.pyc"'
+        ' -prune -o -type f -name "*.hi"'
+        ' -prune -o -type f -name "*.beam"'
+        ' -prune -o -type f -name "*.png"'
+        ' -prune -o -type f -name "*.gif"'
+        ' -prune -o -type f -name "*.jp*g"'
+        ' -prune -o -type f -name "*.ico"'
+        ' -prune -o -type f -name "*.ttf"'
+        ' -prune -o -type f -name "*.zip"'
+        ' -prune -o -type f -name "*.jar"'
+        ' -prune -o -type f -name "*.dot"'
+        ' -prune -o -type f -name "*.pdf"'
+        ' -prune -o -type f -name "*.wav"'
+        ' -prune -o -type f -name "*.mp[34]"'
+        ' -prune -o -type f -name "*.svg"'
+        ' -prune -o -type f -name "*.flip"'
+        ' -prune -o -type f -name "*.class"'
+        ' -prune -o -type f -name "*.gem"'
+        ' -prune -o -type f -name "*.jad"'
+        ' -prune -o -type d -name .idea'
+        ' -prune -o -type f -name "*.iml"'
+        ' -prune -o -type f -name "*.log"'
+        ' -prune -o -type f -name "*"'
+        ' -exec node_modules/.bin/editorconfig-tools check {} \\;')
+
 
 @task
 def xmllint():
-  run('find . -name "*.xml" -exec xmllint --noout {} 2>&1 \\;')
+    run('find . -name "*.xml" -exec xmllint --noout {} 2>&1 \\;')
+
 
 @task(pre=[pep8, pylint, pyflakes, editorconfig, xmllint])
 def lint():
-  pass
+    pass
